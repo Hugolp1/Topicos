af178a5201840f1afa71351595fd3b3919f32e65
Hugo Prado <hugolourencoprado@gmail.com>
2024-03-14 10:53:09 -0300

    Adicionando o projeto de ambientação e ordenação no repositório

---
+++ Ordenacao/ListaExercicios/Ex02b/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Ex02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[100];
+        Random random = new Random();
+        //Preenchendo o vetor com valores aleatórios
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = random.Next(100);
+        }
+        //Imprimindo o vetor desordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine("\n");
+
+        //Ordenar o vetor com um recurso da linguagem
+        Array.Sort(vetor);
+
+        //Imprimindo o vetor ordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
